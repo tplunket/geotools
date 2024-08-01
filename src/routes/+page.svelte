1f33784021293707e6453b77e2347a548c33f783
Tom Plunket <tom@mightysprite.com>
2024-07-31 23:12:23 -0700

    Initial working version.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Point } from '$lib/types';
+    import Map from '../components/Map.svelte';
+
+    type State = {
+        latitude: string,
+        longitude: string,
+        points: Point[]
+    }
+    let my_state = $state<State>({
+        latitude: '',
+        longitude: '',
+        points: []
+    });
+
+    function addPoint() {
+        const { latitude, longitude } = my_state;
+        if (latitude && longitude) {
+            my_state.points.push(
+                { latitude: parseFloat(latitude), longitude: parseFloat(longitude) }
+            );
+            my_state.latitude = '';
+            my_state.longitude = '';
+            console.log(`addpoint, now ${my_state.points.length} long`)
+        }
+    }
+</script>
+
+<style>
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+    }
+
+    .form {
+        margin-bottom: 20px;
+    }
+</style>
+
+<div class="container">
+    <div class="form">
+        <input type="text" placeholder="Latitude" bind:value={my_state.latitude}>
+        <input type="text" placeholder="Longitude" bind:value={my_state.longitude}>
+        <button onclick={addPoint}>Add Point</button>
+    </div>
+    <Map points={my_state.points} />
+</div>
